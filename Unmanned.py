5214a092a5f1b4d3458123c2f13d15de39bc3e83
sancho-mgn <sanchomgn@gmail.com>
2021-04-27 07:02:44 +0300

    changed Unmanned.py

--- Unmanned.py
+++ Unmanned.py
@@ -2,18 +2,19 @@ def Unmanned(L, N, track):
     total_dist = L
     red_add_time = 0
     for i in range(N):
-        travel_time, time_red, time_green = track[i][0], track[i][1], track[i][2]
-        total_wait_time = travel_time + red_add_time
-        total_traf_light = time_red + time_green
+        travel_time, time_red, time_green = [item for item in track[i]]
+        total_wait_time = travel_time + red_add_time # общее время в пути вместе с простоем на красном свете
+        total_traf_light = time_red + time_green # общее время работы светофора
 
-        if total_traf_light > travel_time and L > travel_time:
-             cur_time_lights = total_wait_time % total_traf_light
+        if total_traf_light and L > travel_time: # проверяем что время работы светофора не ноль, и что до светофора мы доедем
+             cur_time_lights = total_wait_time % total_traf_light # проверяем попадаем ли мы под светофор, с учётом пройденных ранее светофоров
              if cur_time_lights < time_red:
-                 red_add_time += time_red - cur_time_lights
+                 red_add_time += time_red - cur_time_lights # вычисляем общее время простоя на красном свете светофоров
     total_dist += red_add_time
     return total_dist
 
 #L = 10
 #N = 2
-#track = [[2, 5, 5], [5, 2, 2]]
+#track = [[3, 5, 5], [5, 2, 2]]
+#track = [[3, 6, 2], [6, 2, 2]]
 #print(Unmanned(L, N, track))
