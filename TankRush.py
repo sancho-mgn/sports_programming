8332003ce73b083c8d7cf0185729294b13ef1c5d
sancho-mgn <sanchomgn@gmail.com>
2021-04-27 14:03:56 +0300

    changed TankRush.py

--- TankRush.py
+++ TankRush.py
@@ -2,7 +2,10 @@ def TankRush(H1, W1, S1, H2, W2, S2):
     L1 = S1.split()
     L2 = S2.split()
     tmp = []
-    if (H1 * W1) % (H2 * W2) != 0:
+    if (H1 <= 0 or H2 <= 0 or W1 <= 0 or W2 <= 0):
+        return False
+
+    if (H2 > H1 or W2 > W1):
         return False
 
     for i in range(len(L1)):
@@ -15,3 +18,5 @@ def TankRush(H1, W1, S1, H2, W2, S2):
         return True
     else:
         return False
+
+#TankRush(5, 15, '000000000000000 000000000000000 000000100000000 000000000000000 000000000000000', 3, 5,'00000 00000 00001')
