4a7583da980ceacf7fcfe30b030c72e21162e126
sancho-mgn <sanchomgn@gmail.com>
2021-04-28 14:40:38 +0300

    changed TankRush.py TankRush_test.py

--- TankRush.py
+++ TankRush.py
@@ -1,26 +1,24 @@
+def search(S1, start_str, end_str, start_col, end_col):
+    S11 = S1[start_str:end_str]
+    for i in range(len(S11)):
+        S11[i] = S11[i][start_col:end_col]
+    return S11
+
 def TankRush(H1, W1, S1, H2, W2, S2):
-    L1 = S1.split()
-    L2 = S2.split()
-    tmp = []
-    if H1 * W1 % H2 * W2 != 0:
-        return False
+    S1 = S1.split()
+    S2 = S2.split()
 
-    if (H1 <= 0 or H2 <= 0 or W1 <= 0 or W2 <= 0):
-        return False
-
-    if (H2 > H1 or W2 > W1):
-        return False
-
-    for i in range(len(L1)):
-        for j in range(len(L2)):
-            if L2[j] in L1[i]:
-                tmp.append(L2[j])
-    tmp = set(tmp)
-    res = all(item in L2 for item in tmp)
-    if res:
-        return True
-    else:
-        return False
-
-#TankRush(5, 15, '000000000000000 000000000000000 000000100000000 000000000000000 000000000000000', 3, 5,'00000 00000 00001')
-#print(TankRush(3,3, '321 694 798', 2, 2, '69 98'))
+    flag = False
+    for i in range((H1 - H2) + 1):
+        j = 0
+        while j in range(W1):
+            start_index = S1[i].find(S2[0], j)
+            if start_index != -1:
+                end_index = start_index + W2
+                end_str = i + H2
+                S11 = search(S1, i, end_str, start_index, end_index)
+                if S11 == S2:
+                    flag = True
+                    break
+            j += 1
+    return flag
--- TankRush_test.py
+++ TankRush_test.py
@@ -7,5 +7,25 @@ class TankRushTest(unittest.TestCase):
     def test1(self):
         self.assertEqual(TankRush.TankRush(5,15,'000000000000000 000000000000000 000000100000000 000000000000000 000000000000000',3,5,'00000 00000 00001'), True)
 
+    def test2(self):
+        self.assertEqual(TankRush.TankRush(15,15,'900934352126360 119214144058652 979486082875698 322436531185165 887105930987956 232802644488782 302771989566798 073573207654780 311755785362806 909007939272309 395094805516080 562910805349811 993854324744973 768703404219199 630625270887199',2,2,'99 99'), True)
+
+    def test3(self):
+        self.assertEqual(TankRush.TankRush(3, 3, '321 694 798', 3, 2, '32 69 79'), True)
+
+    def test4(self):
+        self.assertEqual(TankRush.TankRush(7, 3, '727 158 735 425 235 123 432',
+                                            6, 2, '72 15 73 42 23 12'), True)
+
+    def test5(self):
+        self.assertEqual(TankRush.TankRush(3, 4, "1234 2345 0987", 2, 2, "23 98"), False)
+
+    def test6(self):
+        self.assertEqual(TankRush.TankRush(3, 8, '7277604 1583078 1427735', 3, 2, '78 35 20'), False)
+
+    def test7(self):
+        self.assertEqual(TankRush.TankRush(3, 15, '993854324744973 768703404219199 630625270887199',
+                                            2, 2, '99 99'), True)
+
 if __name__ == '__main__':
     unittest.main()
