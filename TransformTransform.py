59875d2a3db00f494b8f1ff7f3fd241242701874
sancho-mgn <sanchomgn@gmail.com>
2021-05-26 07:29:48 +0300

    edit TransformTransform.py

--- TransformTransform.py
+++ TransformTransform.py
@@ -1,11 +1,11 @@
 def TransformTransform(A, N):
-    return sum(S(S(A, N), N)) % 2 == 0
+    return sum(S(S(A))) % 2 == 0
 
 
-def S(A, N):
+def S(A):
     B = []
-    for i in range(N):
-        for j in range(N - i):
+    for i in range(len(A)):
+        for j in range(len(A) - i):
             k = i + j
             B.append(max(A[j:k + 1]))
     return B
